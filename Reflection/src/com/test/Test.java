7d9484fb8eb7f26539b888636ed2918582a9577f
DIPAK AHER <aherdipak3@gmail.com>
2019-03-30 13:54:56 +0530

    Getting filed name and value

--- Reflection/src/com/test/Test.java
+++ Reflection/src/com/test/Test.java
@@ -1,8 +1,106 @@
 package com.test;
 
+import java.lang.reflect.Field;
+
+
 public class Test {
 
-	public static void main(String[] args) {
-		System.out.println("test");
+	public void printFiledAndValue(Object obj) throws Exception{
+		Class clazz = obj.getClass();
+		 Field[] fields = clazz.getDeclaredFields();
+		    for(Field field : fields)
+		    {
+		        field.setAccessible(true);
+		        System.out.println("Filed Name: "+field.getName() +"  --> "+field.get(obj));
+		        if (!field.getType().isPrimitive() && !field.getType().getName().contains("java.lang"))
+		        {
+		        	printFiledAndValue(field.get(obj));
+		        }
+		    }
 	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		User u1=new User("BHARAT",25,"AAA");
+		
+		Human human = new Human();
+		human.setHumanName("KARAN");
+		human.setUser(u1);
+		
+		TestClass testClass = new TestClass();
+		testClass.setHuman(human);
+		
+		new Test().printFiledAndValue(testClass);
+		
+	}
+}
+
+
+
+
+class TestClass{
+	Human human;
+	byte byteVar = 5;
+    short shortVar = 20;
+    int intVar = 30;
+    long longVar = 60;
+    float floatVar = 20;
+    double doubleVar = 20.123;
+    boolean booleanVar = true;
+    char charVar ='W';
+    String str = "string";
+    
+	public Human getHuman() {
+		return human;
+	}
+	public void setHuman(Human human) {
+		this.human = human;
+	}
+   
+}
+
+class Human{
+	private String humanName;
+	private User user;
+	public String getHumanName() {
+		return humanName;
+	}
+	public void setHumanName(String humanName) {
+		this.humanName = humanName;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
+
+class User{
+	private String userName;
+	private int userAge;
+	private String fname;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public int getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+	public User(String userName,int userAge,String fname) {
+		this.userName=userName;
+		this.userAge=userAge;
+		this.fname = fname;
+	}
+	public User() {}
+	@Override
+	public String toString() {
+		return "userName=" + userName + ", userAge=" + userAge +", fname=" + fname ;
+	}
+	
 }
---
+++ Reflection/src/com/test/Test2.java
@@ -0,0 +1,134 @@
+package com.test;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Test2 {
+
+	public Object convertDataTypeOfFiledValue(Field field,Object value) {
+		//System.out.println(field.getType().getName().substring(field.getType().getName().lastIndexOf(".")+1));
+		if(field.getType().getName().substring(field.getType().getName().lastIndexOf(".")+1).equalsIgnoreCase("String")) {
+			return String.valueOf(value);
+		}else if(field.getType().getName().substring(field.getType().getName().lastIndexOf(".")+1).equalsIgnoreCase("Integer")|| field.getType().getName().substring(field.getType().getName().lastIndexOf(".")+1).equalsIgnoreCase("int")) {
+			return (int)value;
+		}
+		return null;
+	}
+	
+	public void convertMasterToTemp(Object master,Object temp) throws Exception{
+		Map<String,Object> masterValMap =new HashMap<>();
+		try {
+			Class mclass = master.getClass();
+			Field [] mFields = mclass.getDeclaredFields();
+			
+			for (Field mField : mFields) {
+				mField.setAccessible(true);
+				if(mField.get(master)!= null)
+					masterValMap.put(mField.getName(), mField.get(master));
+			}
+			
+			Class tclass = temp.getClass();
+			Field [] tFields = tclass.getDeclaredFields();
+			
+			for (Field tField : tFields) {
+				tField.setAccessible(true);
+				if(masterValMap.get(tField.getName())!= null) {
+					Object convertedVal = convertDataTypeOfFiledValue(tField,masterValMap.get(tField.getName()));
+					if(convertedVal !=null) {
+						tField.set(temp, convertedVal);
+					}
+				}
+					
+			}
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	
+	public void printFiledAndValue(Object obj) throws Exception{
+		Class clazz = obj.getClass();
+		 Field[] fields = clazz.getDeclaredFields();
+		    for(Field field : fields)
+		    {
+		        field.setAccessible(true);
+		        System.out.println("Filed Name: "+field.getName() +"  --> "+field.get(obj));
+		        if (!field.getType().isPrimitive() && !field.getType().getName().contains("java.lang"))
+		        {
+		        	printFiledAndValue(field.get(obj));
+		        }
+		    }
+	}
+	
+	public static void main(String[] args) throws Exception {
+		User_1 u1=new User_1("deepak!",25,"AAA");
+		User_2 u2=new User_2();
+		
+		System.out.println("------- BEFORE ---------");
+		
+		System.out.println("U1: "+u1);
+		System.out.println("U2: "+u2);
+		
+		new Test2(). convertMasterToTemp(u1,u2);
+		
+		System.out.println("------- AFTER ---------");
+		
+		System.out.println("U1: "+u1);
+		System.out.println("U2: "+u2);
+		
+	}
+}
+
+
+class User_1{
+	private String userName;
+	private int userAge;
+	private String fname;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public int getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+	public User_1(String userName,int userAge,String fname) {
+		this.userName=userName;
+		this.userAge=userAge;
+		this.fname = fname;
+	}
+	public User_1() {}
+	@Override
+	public String toString() {
+		return "userName=" + userName + ", userAge=" + userAge +", fname=" + fname ;
+	}
+	
+}
+
+
+class User_2{
+	private String userName;
+	private int userAge;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Integer getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+	@Override
+	public String toString() {
+		return "userName=" + userName + ", userAge=" + userAge ;
+	}
+}
